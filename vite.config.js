b91eb1271faa07dcd2019a6935af872076c001a2
yuichi.nagakura <yuichi.nagakura@asia-quest.jp>
2022-02-02 01:27:48 +0900

    setting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/fullcalendar-demo/dist/',
   build: {
     chunkSizeWarningLimit: 1500,
     minify: true,
