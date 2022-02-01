51d459bd0ccf9ec031524a1016f79779b1e1e2cd
yuichi.nagakura <yuichi.nagakura@asia-quest.jp>
2022-02-02 00:56:18 +0900

    docs配下を公開するように修正

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/docs/',
   build: {
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
